a1cc1bd1d4f34e26db663b231bbaad0abfbbd6f5
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-23 13:28:57 +0200

    some more info in basic car class, Car.getMaxGear() made abstract a subclass for a particular car initial work

--- src/com/accenture/bootcamp/task3/Car.java
+++ src/com/accenture/bootcamp/task3/Car.java
@@ -1,21 +1,33 @@
 package com.accenture.bootcamp.task3;
 
-public class Car extends Vehicle {
+import static com.accenture.bootcamp.task3.Car.Fuel.*;
+
+public abstract class Car extends Vehicle {
+
+
+    public enum Fuel{
+        PETROL,DIESEL,NATURAL_GAS,ELECTRIC,EXOTIC
+    }
     public static final int NEUTRAL_GEAR=0;
     public static final int REVERSE_GEAR=-1;
-    private final int maxGear=5;
     private int currentGear=NEUTRAL_GEAR;
+    private String model="Generic car";
+    private String engine="Generic engine";
+    private Car.Fuel fuel=PETROL;
 
 
-    public int getMaxGear() {
-        return maxGear;
-    }
+
+    /**
+     * todo cars with multiple reverse gears
+     * @return maximum gear this car has, not counting the neutral and reverse. 1 for continuous automatic transmissions.
+     */
+    public abstract int getMaxGear();
 
     public int getCurrentGear() {
         return currentGear;
     }
     public void shiftUp(){
-        if(currentGear<maxGear)
+        if(currentGear<getMaxGear())
             currentGear++;
     }
     public void shiftDown(){
@@ -28,6 +40,17 @@ public class Car extends Vehicle {
     }
     public void reverse(){
         currentGear=REVERSE_GEAR;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public Fuel getFuel() {
+        return fuel;
     }
 
 }
---
+++ src/com/accenture/bootcamp/task3/VWGolfMk810TSI.java
@@ -0,0 +1,9 @@
+package com.accenture.bootcamp.task3;
+
+public class VWGolfMk810TSI extends Car {
+
+    @Override
+    public int getMaxGear() {
+        return 6;
+    }
+}
