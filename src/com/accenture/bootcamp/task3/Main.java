fe87f0041a96a29a8af7b896a20278b3657ae821
AleksandrsC <mirrorfrequency@gmail.com>
2022-03-23 10:59:13 +0200

    bank account complete with demo/test cases.

--- src/com/accenture/bootcamp/task3/Main.java
+++ src/com/accenture/bootcamp/task3/Main.java
@@ -4,5 +4,53 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("BankAccount demo");
+        BankAccount a=new BankAccount(2000);
+        BankAccount b=new BankAccount(2000);
+        //
+        System.out.println("account A");
+        a.printBalance();
+        System.out.println("account B");
+        b.printBalance();
+        //
+        System.out.println("deposit A 1000.1");
+        a.deposit(1000.1);
+        System.out.println("account A");
+        a.printBalance();
+        //
+        System.out.println("deposit A 7000.1");
+        a.deposit(7000.1);
+        System.out.println("account A");
+        a.printBalance();
+        //
+        System.out.println("withdraw A 700.1");
+        a.withdraw(700.1);
+        System.out.println("account A");
+        a.printBalance();
+        //
+        System.out.println("withdraw A 700000");
+        a.withdraw(700000);
+        System.out.println("account A");
+        a.printBalance();
+        //
+        System.out.println("transfer B to A 10");
+        a.transferFrom(b,10);
+        System.out.println("account A");
+        a.printBalance();
+        System.out.println("account B");
+        b.printBalance();
+        //
+        System.out.println("transfer B to A 10000");
+        a.transferFrom(b,10000);
+        System.out.println("account A");
+        a.printBalance();
+        System.out.println("account B");
+        b.printBalance();
+
+
+
+
+
+
     }
 }
